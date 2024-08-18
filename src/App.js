36c2f16647d36e2a76b961df851b0d0adca9b16c
Anas Hany <anashany219@gmail.com>
2024-08-18 22:16:23 +0300

    🚑 useEffect to the rescue for side effects management

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -74,9 +74,17 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const key = "59c51f23";
+
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://www.omdbapi.com/?apikey=${key}&s=Transformers`)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search));
+  }, []);
 
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StarRatings from "./StarRatings";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRatings maxRating={5} />
+    <App />
   </React.StrictMode>
 );
